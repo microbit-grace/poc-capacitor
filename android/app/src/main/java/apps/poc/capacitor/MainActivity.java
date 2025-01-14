54e6db5057a2ee932275b2b398775031053dd512
Grace <grace@microbit.org>
2025-01-14 16:34:41 +0000

    Add capacitor to project

---
+++ android/app/src/main/java/apps/poc/capacitor/MainActivity.java
@@ -0,0 +1,5 @@
+package apps.poc.capacitor;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'apps.poc.capacitor',
+  appName: 'apps-poc-capacitor',
+  webDir: 'dist'
+};
+
+export default config;
