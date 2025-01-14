763031ea5e36d563fbd225edd7d5e76d79f22d29
Grace <grace@microbit.org>
2025-01-14 16:22:56 +0000

    Embed MakeCode skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,48 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useCallback, useState } from "react";
 import "./App.css";
+import { MakeCodeFrame, Project } from "@microbit/makecode-embed/react";
+
+const starterProject = {
+  text: {
+    "main.blocks":
+      '<xml xmlns="http://www.w3.org/1999/xhtml">\n  <variables></variables>\n</xml>',
+    "main.ts": "\n",
+    "README.md": " ",
+    "pxt.json":
+      '{\n    "name": "Untitled",\n    "dependencies": {\n        "core": "*"\n , "radio": "*"\n     },\n    "description": "",\n    "files": [\n        "main.blocks",\n        "main.ts",\n        "README.md"\n    ],\n    "preferredEditor": "blocksprj"\n}',
+  },
+} as Project;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [open, setOpen] = useState<boolean>(false);
+  const initialProject = useCallback(async () => [starterProject], []);
+  const handleClose = useCallback(() => {
+    setOpen(false);
+  }, []);
+  const flashMicrobit = useCallback(
+    (download: { name: string; hex: string }) => {
+      console.log(download);
+      setOpen(true);
+    },
+    []
+  );
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <dialog open={open} onCancel={handleClose}>
+        <h1 style={{ fontSize: 20 }}>Flash micro:bit?</h1>
+        <div style={{ display: "flex", gap: "10px" }}>
+          <button>Connect to micro:bit</button>
+          <button onClick={handleClose}>Close</button>
+        </div>
+      </dialog>
+      <MakeCodeFrame
+        style={{ height: "100%" }}
+        controller={2}
+        loading="eager"
+        initialProjects={initialProject}
+        onDownload={flashMicrobit}
+      />
     </>
   );
 }
